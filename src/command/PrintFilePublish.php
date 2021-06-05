ca02d7e53f300008505b5809f2aaf6fa7ec2ee9c
jhz-code <zhuok520@qq.com>
2021-06-05 19:50:31 +0800

    publish

--- src/command/PrintFilePublish.php
+++ src/command/PrintFilePublish.php
@@ -4,7 +4,47 @@
 namespace RmTop\command;
 
 
-class PrintFilePublish
+use RmTop\lib\PublishFile;
+use think\console\Command;
+use think\console\Input;
+use think\console\input\Argument;
+use think\console\Output;
+use think\Exception;
+
+class PrintFilePublish extends Command
 {
+
+
+
+    protected function configure()
+    {
+        $this->setName('rmtop:publish_printer')
+            ->setDescription('printer publish');
+    }
+
+
+    /**
+     * 执行数据
+     * @param Input $input
+     * @param Output $output
+     * @return int|void|null
+     * @throws \ReflectionException
+     */
+    protected function execute(Input $input, Output $output)
+    {
+
+        try{
+            PublishFile::PublishFileToSys($output);//发布文件
+            $output->writeln("all publish successfully！");
+        }catch (Exception $exception){
+            $output->writeln($exception->getMessage());
+        }
+
+    }
+
+
+
+
+
 
 }
---
+++ src/lib/PublishFile.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace RmTop\lib;
+
+
+class PublishFile
+{
+
+    /**
+     * 发布到系统
+     */
+    static function PublishFileToSys($output){
+        $destination = root_path() . '/database/migrations/';
+        if(!is_dir($destination)){
+            mkdir($destination, 0755, true);
+        }
+        $source = dirname(__DIR__).'/database/migrations/';
+        $handle = dir($source);
+        while($entry=$handle->read()) {
+            if(($entry!=".")&&($entry!="..")){
+                if(is_file($source.$entry)){
+                    copy($source.$entry, $destination.$entry);
+                    $output->writeln("$source.$entry --- publish successfully！");
+                }
+            }
+        }
+
+
+        if (!file_exists(config_path().'rmtop.conf')) {
+            copy(dirname(__DIR__).'/config/rmtop.php', config_path().'rmtop.php');
+        }
+    }
+
+}
