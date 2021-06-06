6191b6daab1af70b6e5e4f23899be4b79fe157f5
jhz-code <zhuok520@qq.com>
2021-06-06 11:51:09 +0800

    打印机列队

--- src/core/TopFeier.php
+++ src/core/TopFeier.php
@@ -10,30 +10,32 @@
 namespace RmTop\RmPrinter\core;
 
 
+use RmTop\RmPrinter\job\PrinterJob;
 use RmTop\RmPrinter\lib\feier\FeierCloudPrint;
 use RmTop\RmPrinter\model\PrinterModel;
 use think\db\exception\DataNotFoundException;
 use think\db\exception\DbException;
 use think\db\exception\ModelNotFoundException;
 use think\Exception;
+use think\facade\Queue;
 
 class TopFeier
 {
 
-
     /**
      * 让所有打印机执行打印
      * @param string $type
+     * @param bool $job
      * @param array $where
      * @param string $filed
      * @param $content
      * @param int $times
-     * @throws Exception
      * @throws DataNotFoundException
      * @throws DbException
+     * @throws Exception
      * @throws ModelNotFoundException
      */
-    function allPrinterRun(string $type, array $where ,string $filed,$content,int $times){
+    function allPrinterRun(string $type,bool $job = false, array $where ,string $filed,$content,int $times){
         if($where){
             $printerList = PrinterModel::where(1)->field($filed)->select();
         }else{
@@ -42,10 +44,17 @@ class TopFeier
         if($printerList){
             $printer = new FeierCloudPrint();
             foreach ($printerList as $key=>$item){
-                if($type == 'receipt'){
-                    $printer->printReceipt($item['print_sn'],$content,$times);
-                }else if($type == 'receipt'){
-                    $printer->printLabel($item['print_sn'],$content,$times);
+                //列队操作
+                if($job){
+                    $item['type'] = $type ;
+                    Queue::push(PrinterJob::class,$item);
+                }else{
+                //直接打印
+                    if($type == 'receipt'){
+                        $printer->printReceipt($item['print_sn'],$content,$times);
+                    }else if($type == 'label'){
+                        $printer->printLabel($item['print_sn'],$content,$times);
+                    }
                 }
             }
         }
@@ -63,14 +72,23 @@ class TopFeier
      * @throws DbException
      * @throws ModelNotFoundException
      */
-    function singlePrinter(string $type,array $where,$content,int $times){
+    function singlePrinter(string $type,bool $job = false,array $where,$content,int $times){
          $printerInfo = PrinterModel::where($where)->find();
          if($printerInfo){
-              $printer = new FeierCloudPrint();
-             if($type == 'receipt'){
-                 $printer->printReceipt($printerInfo['print_sn'],$content,$times);
-             }else if($type == 'label'){
-                 $printer->printLabel($printerInfo['print_sn'],$content,$times);
+             $printer = new FeierCloudPrint();
+             //列队操作
+             if($job){
+                 $item['type'] = $type ;
+                 $item['content'] = $content ;
+                 $item['times'] = $times ;
+                 Queue::push(PrinterJob::class,$item);
+             }else{
+                 //直接打印
+                 if($type == 'receipt'){
+                     $printer->printReceipt($printerInfo['print_sn'],$content,$times);
+                 }else if($type == 'label'){
+                     $printer->printLabel($printerInfo['print_sn'],$content,$times);
+                 }
              }
          }
     }
---
+++ src/job/PrinterJob.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by YnRmsf.
+ * User: zhuok520@qq.com
+ * Date: 2021/6/6
+ * Time: 11:28 上午
+ */
+
+
+namespace RmTop\RmPrinter\job;
+
+
+use RmTop\RmPrinter\lib\feier\FeierCloudPrint;
+use think\queue\Job;
+
+/**
+ * 创建列队打印任务
+ * Class PrinterJob
+ * @package RmTop\RmPrinter\job
+ */
+
+class PrinterJob
+{
+
+    public function fire(Job $job, $data){
+
+        //....这里执行具体的任务
+
+        if($job->attempts() > 3) {
+            //通过这个方法可以检查这个任务已经重试了几次了
+            $job->release(300);
+        }
+        if($this->printerDone($data)){
+            //如果任务执行成功后 记得删除任务，不然这个任务会重复执行，直到达到最大重试次数后失败后，执行failed方法
+            $job->delete();
+        }
+        // 也可以重新发布这个任务
+     //   $job->release($delay); //$delay为延迟时间
+
+    }
+
+
+    /**
+     * @param $data
+     * @return bool
+     * @throws \think\Exception
+     */
+    function printerDone($data): bool
+    {
+        //直接打印
+        $printer = new FeierCloudPrint();
+        if('receipt' == $data['receipt']){
+            $printer->printReceipt($data['print_sn'],$data['content'],$data['times']);
+        }else if('label' == $data['label']){
+            $printer->printReceipt($data['print_sn'],$data['content'],$data['times']);
+        }
+        return true;
+    }
+
+
+    public function failed($data){
+        // ...任务达到最大重试次数后，失败了
+    }
+
+
+}
